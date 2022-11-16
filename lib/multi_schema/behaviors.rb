6bcf3a65be1db6ddf082536f865cee4b7820d98d
Neil Duff-Howie <neil.howie@yakara.com>
2022-11-16 10:44:19 +0000

    Fix for later postgres, clear AR cache on switch

--- lib/multi_schema/behaviors.rb
+++ lib/multi_schema/behaviors.rb
@@ -38,7 +38,7 @@ module MultiSchema
 
     def all_schemas
       ActiveRecord::Base.connection.select_values <<-END
-      SELECT *
+      SELECT nspname
       FROM pg_namespace
       WHERE
         nspname NOT IN ('information_schema') AND
@@ -51,8 +51,10 @@ module MultiSchema
     end
 
     def reset_schema_path
-      puts "--- Restore Schema to #{@@default_search_path}" unless disable_message
-      ActiveRecord::Base.connection.schema_search_path = @@default_search_path
+      puts '--- Restore Schema to "$user", public' unless disable_message
+      ::ActiveRecord::Base.connection.schema_search_path = '"$user", public'
+      clear_cache
+      nil
     end
 
     def schema_path
@@ -62,6 +64,12 @@ module MultiSchema
     def set_schema_path(schema)
       puts "--- Select Schema: #{schema} " unless disable_message
       ActiveRecord::Base.connection.schema_search_path = schema
+      clear_cache
+      nil
+    end
+
+    def set_schema_and_public(schema)
+      set_schema_path "#{schema}, public"
     end
 
     def push_schema_to_path(schema)
@@ -72,6 +80,34 @@ module MultiSchema
     def pop_schema_from_path
       new_path = schema_path.sub(/\w,/, '')
       set_schema_path(new_path)
+    end
+
+    def ensure_schema_reset
+      old = ::ActiveRecord::Base.connection.schema_search_path
+
+      begin
+        yield
+      ensure
+        e = $ERROR_INFO
+        e = e.cause while e.respond_to?(:cause)
+        set_schema_path old unless e.is_a?(PG::Error)
+      end
+    end
+
+    def with_schema_and_public(schema)
+      ensure_schema_reset do
+        set_schema_and_public schema
+        yield
+      end
+    end
+
+    def each_schema_and_public
+      ensure_schema_reset do
+        (all_schemas - ['public']).each do |schema|
+          set_schema_and_public schema
+          yield schema
+        end
+      end
     end
 
     private
@@ -88,6 +124,19 @@ module MultiSchema
       input.map do |item|
         unify_type item, type, &block
       end
+    end
+
+    def clear_cache
+      ::ActiveRecord::Base.connection.schema_cache.clear!
+
+      ::ActiveRecord::Base.descendants.map(&:base_class).uniq.each do |klass|
+        # reset_sequence_name would needlessly fetch them all now.
+        unless klass.instance_variable_get(:@explicit_sequence_name)
+          klass.instance_variable_set(:@sequence_name, nil)
+        end
+      end
+
+      nil
     end
   end
 end
